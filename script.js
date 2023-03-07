6e841ff23db69568b9c16aab15cd998f9477b80b
Anjali Peddawad <104055987+anjalipeddawad27@users.noreply.github.com>
2023-03-07 21:02:51 +0530

    Add files via upload
    Portfolio:
    The portfolio topic is Resume made using HTML,CSS, JavaScript.
    Greate Logic using and styling of css.

---
+++ script.js
@@ -0,0 +1,16 @@
+console.log("Script Running...")
+document.querySelector('.cross').style.display='none';
+document.querySelector('.humburger').addEventListener("click", ()=>{
+    document.querySelector('.sidebar').classList.toggle('sidebarGo');
+    if(document.querySelector('.sidebar').classList.contains('sidebarGo')){
+        document.querySelector('.hum').style.display='inline'
+        document.querySelector('.cross').style.display='none'
+    }
+    else{
+        document.querySelector('.hum').style.display='none'
+        setTimeout(()=>{
+            document.querySelector('.cross').style.display='inline'
+
+        },350);
+    }
+})
